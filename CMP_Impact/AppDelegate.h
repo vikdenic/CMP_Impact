91071db8e9072dc186ec62d7f266010b443101ab
vik denic <vik.denic@gmail.com>
2015-07-02 19:06:27 -0500

    Initial Commit

---
+++ CMP_Impact/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CMP_Impact
+//
+//  Created by Vik Denic on 7/2/15.
+//  Copyright (c) 2015 Vik Denic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
