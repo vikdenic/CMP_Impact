d20b33b6a9380b2360ef30c1f606fb4e0b47f705
vik denic <vik.denic@gmail.com>
2015-07-02 20:17:53 -0500

    setup loginvc ui

---
+++ CMP_Impact/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  CMP_Impact
+//
+//  Created by Vik Denic on 7/2/15.
+//  Copyright (c) 2015 Vik Denic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
---
+++ CMP_Impact/LoginViewController.m
@@ -0,0 +1,33 @@
+//
+//  LoginViewController.m
+//  CMP_Impact
+//
+//  Created by Vik Denic on 7/2/15.
+//  Copyright (c) 2015 Vik Denic. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@property (strong, nonatomic) IBOutlet UITextField *usernameTextfield;
+@property (strong, nonatomic) IBOutlet UITextField *passwordTextfield;
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (IBAction)onSegmentSelected:(UISegmentedControl *)sender
+{
+}
+
+- (IBAction)onLoginTapped:(UIButton *)sender
+{
+}
+
+@end
